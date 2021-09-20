7ee65044bbef8d5010b6086b43c1ae939b0d28de
rccs09 <rccs09@gmail.com>
2021-09-19 21:04:44 -0500

    fin merge con remoto

---
+++ src/helpers/prueba.js
@@ -0,0 +1,48 @@
+const PendingAliquots = require('./pendingAliquots');
+const Util = require('../config/util');
+const Constants = require('../config/constants');
+
+const pruebaCodigo = async() => {
+    const from = "593995711770";
+    //const from = "593987416406";
+    //const from = "593990102302";
+    //const from = "59312345678";
+    const pendingAliquots = new PendingAliquots(Constants.FILE_PATH);
+    await pendingAliquots.init();
+
+    //console.log(pendingAliquots.jsonXlsIncome);
+    console.log("******✅ ⚠️ ❌ pruebas***********************");
+    
+    
+    const house = "CASA 29"
+    //console.log(pendingAliquots.generateMessagePendingByHouse(house));
+    console.log(pendingAliquots.generateDetailPaysMessages(house));
+
+
+    //=========================================
+    /*
+    const regNumbers =   pendingAliquots.validateFromNumber(from);
+    if(regNumbers){
+        console.log(regNumbers);
+        const messageTest = pendingAliquots.generateMessagePendingByHouse(regNumbers[Constants.SHT_TELF_HOUSE_LABEL]);
+        console.log(messageTest);
+
+        if(regNumbers[Constants.SHT_TELF_CELL1_LABEL]?.length > 0){
+            console.log(regNumbers[Constants.SHT_TELF_CELL1_LABEL]);
+        }
+        if(regNumbers[Constants.SHT_TELF_CELL2_LABEL]?.length > 0){
+            console.log(regNumbers[Constants.SHT_TELF_CELL2_LABEL]);
+        }
+    }else{
+        console.log("Su numero no esta registrado");
+    }
+    */
+    
+    //============================
+
+
+}
+
+
+module.exports = {pruebaCodigo};
+
